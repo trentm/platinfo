b58202e9e9e29d7b0d295e2a302b96e89df520f5
trentm <trentm@a05a7910-2463-11de-96e2-9591d3992c9b>
2009-09-24 21:57:05 +0000

    platinfo2 should be a 2.x version :)

--- lib/platinfo2.py
+++ lib/platinfo2.py
@@ -75,7 +75,7 @@
 # - YAGNI: Having a "quick/terse" mode. Will always gather all possible
 #   information unless come up with a case to NOT do so.
 
-__version_info__ = (1, 0, 0)
+__version_info__ = (2, 0, 0)
 __version__ = '.'.join(map(str, __version_info__))
 
 import os
